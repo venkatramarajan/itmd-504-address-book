b70712cd38e8daf23c8287d1e160e3c55b7e7d48
Ramarajan Venkat <raja@ramarajan.com>
2025-05-05 18:20:50 -0500

    Update app.py to run on host '0.0.0.0' and port 5000; modify requirements.txt to update Werkzeug, python-dotenv, mysqlclient, and PyMySQL versions for compatibility.

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_admin_user()
-    app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0', port=5000) 
